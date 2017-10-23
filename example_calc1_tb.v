46db95d28bf0b91c9524af1dbb75b5178cc8cee2
Will Rollason <willr292@gmail.com>
2017-10-23 14:01:57 +0100

    beevdcs

--- example_calc1_tb.v
+++ example_calc1_tb.v
@@ -199,7 +199,7 @@ module example_calc1_tb;
 
 	// TEST 9 
 	#400
-	for(x=1; x<(100); x=x+1) begin
+	for(x=1; x<(4294967295); x=x+1) begin
 		
 			#400
 			req1_cmd_in = 1;
@@ -244,14 +244,14 @@ module example_calc1_tb;
 
 
      end // initial begin
-
+     	/*
    always
      @ (reset or req1_cmd_in or req1_data_in or req2_cmd_in or req2_data_in or req3_cmd_in or req3_data_in or req4_cmd_in or req4_data_in) begin
 	
 	$display ("%t: r:%b \n 1c:%d,1d:%d \n 2c:%d,2d:%d \n 3c:%d,3d:%d \n 4c:%d,4d:%d \n 1r:%d,1d:%d \n 2r:%d,2d:%d \n 3r:%d,3d:%d \n 4r:%d,4d:%d \n\n", $time, reset[1], req1_cmd_in, req1_data_in, req2_cmd_in, req2_data_in, req3_cmd_in, req3_data_in, req4_cmd_in, req4_data_in, out_resp1, out_data1, out_resp2, out_data2, out_resp3, out_data3, out_resp4, out_data4);
 	
      end
-
+	*/
 endmodule // example_calc1_tb
 
 
