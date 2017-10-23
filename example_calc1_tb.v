2c87937b405eaeb6fbb4c07164b6871bd007fbfd
Will Rollason <willr292@gmail.com>
2017-10-23 14:43:44 +0100

    j

--- example_calc1_tb.v
+++ example_calc1_tb.v
@@ -15,6 +15,8 @@ module example_calc1_tb;
 	integer x;
 	integer y;
 
+	integer rd_data1_out
+
    initial 
      begin
 	c_clk = 0;
@@ -216,7 +218,7 @@ module example_calc1_tb;
 			req1_data_in = 32'b0000_0000_0000_0000_0000_0000_0000_0000;
 
 			#1000
-			integer rd_data1_out = out_data1;
+			rd_data1_out = out_data1;
 			if(rd_data1_out != x) begin
 				$display("ANSWER WAS NOT CORRECT, out data was %d when answer should be %d\n", out_data1, x);
 				end
