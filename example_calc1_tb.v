a1f453e422e489a0097ffd5c7f10558da8241ea4
Will Rollason <willr292@gmail.com>
2017-10-23 14:41:56 +0100

    bvcdsx

--- example_calc1_tb.v
+++ example_calc1_tb.v
@@ -216,8 +216,8 @@ module example_calc1_tb;
 			req1_data_in = 32'b0000_0000_0000_0000_0000_0000_0000_0000;
 
 			#1000
-
-			if(out_data1 != x) begin
+			int rd_data1_out = out_data1;
+			if(rd_data1_out != x) begin
 				$display("ANSWER WAS NOT CORRECT, out data was %d when answer should be %d\n", out_data1, x);
 				end
 		end
