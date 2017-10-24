f4e97f36326484ad0ce03824127716aa83082b82
Will Rollason <willr292@gmail.com>
2017-10-24 22:10:46 +0100

    gbfdv

--- addition_calc1_tb.v
+++ addition_calc1_tb.v
@@ -1,9 +1,9 @@
 module addition_calc1_tb (
 
-input cmd_in;
-input data_in;
-input clk;
-output data_out;
+input cmd_in,
+input data_in,
+input clk,
+output data_out
 );
 wire [0:31] data_out;
 reg [0:31] data_in;
