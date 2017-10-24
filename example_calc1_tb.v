20f856f56e6cd5b2b82e1143ef60db90eadbc3bd
Will Rollason <willr292@gmail.com>
2017-10-24 15:41:13 +0100

    jijkjknj

--- example_calc1_tb.v
+++ example_calc1_tb.v
@@ -218,7 +218,7 @@ module example_calc1_tb;
 				$display("ANSWER WAS CORRECT, out data was %d and answer is %d\n", rd_data1_out, x);
 				end
 
-      #200
+      #100
 			x=(x<<1);
 		end : add
 
@@ -260,7 +260,7 @@ module example_calc1_tb;
 				$display("ANSWER WAS CORRECT, out data was %d and answer is %d\n", rd_data1_out, (y<<1));
 				end
 
-      #200  
+      #100
 			y=(y<<1);
 		end : leftshift
 
