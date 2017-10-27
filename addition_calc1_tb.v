857e734381e45e145572a64f025616228ff6de48
Will Rollason <willr292@gmail.com>
2017-10-27 12:10:25 +0100

    vretbrvfd

--- addition_calc1_tb.v
+++ addition_calc1_tb.v
@@ -13,13 +13,13 @@ initial begin
 
       #200
 
-      assign cmd_in = 1;
-      assign data_in = x;
+       cmd_in = 1;
+       data_in = x;
 
       #200
 
-      assign cmd_in = 0;
-      assign data_in = 32'b0000_0000_0000_0000_0000_0000_0000_0000;
+       cmd_in = 0;
+       data_in = 32'b0000_0000_0000_0000_0000_0000_0000_0000;
 
       #10
 
