e026b6c4e3ee4feb3513e13465098441424b7dde
Will Rollason <willr292@gmail.com>
2017-10-27 12:53:16 +0100

    hfgxgmhfmh

--- addition_calc1_tb.v
+++ addition_calc1_tb.v
@@ -154,3 +154,103 @@
 
             x=(x<<1);
           end
+
+          // TEST 5: Overflow error on port 1
+        	$display("Test for overflow error on port 1, adding 1 to max\n");
+        	#400
+
+        		req1_cmd_in = 1;
+        	req1_data_in = 32'b1111_1111_1111_1111_1111_1111_1111_1111;
+        	req2_cmd_in = 0;
+        	req2_data_in = 0;
+        	req3_cmd_in = 0;
+        	req3_data_in = 0;
+        	req4_cmd_in = 0;
+        	req4_data_in = 0;
+
+        	#200
+
+        	req1_cmd_in = 0;
+        	req1_data_in = 32'b0000_0000_0000_0000_0000_0000_0000_0001;
+
+        	#10
+        	rd_data1_out = out_data1;
+        	rd_resp1_out = out_resp1;
+          if (rd_data1_out != 2 || rd_data1_out != 0) begin
+            $display("Output response should be 2 and is: %d, and answer should be 0 and is %d\n", rd_resp1_out, rd_data1_out);
+            end
+
+          // TEST 6: Overflow error on port 2
+        	$display("Test for overflow error on port 2, adding 1 to max\n");
+        	#400
+
+        		req1_cmd_in = 0;
+        	req1_data_in = 0;
+        	req2_cmd_in = 1;
+        	req2_data_in = 32'b1111_1111_1111_1111_1111_1111_1111_1111;
+        	req3_cmd_in = 0;
+        	req3_data_in = 0;
+        	req4_cmd_in = 0;
+        	req4_data_in = 0;
+
+        	#200
+
+        	req2_cmd_in = 0;
+        	req2_data_in = 32'b0000_0000_0000_0000_0000_0000_0000_0001;
+
+        	#10
+        	rd_data2_out = out_data2;
+        	rd_resp2_out = out_resp2;
+          if (rd_data2_out != 2 || rd_data2_out != 0) begin
+            $display("Output response should be 2 and is: %d, and answer should be 0 and is %d\n", rd_resp2_out, rd_data2_out);
+            end
+
+          // TEST 7: Overflow error on port 3
+          $display("Test for overflow error on port 3, adding 1 to max\n");
+          #400
+
+            req1_cmd_in = 0;
+          req1_data_in = 0;
+          req2_cmd_in = 0;
+          req2_data_in = 0;
+          req3_cmd_in = 1;
+          req3_data_in = 32'b1111_1111_1111_1111_1111_1111_1111_1111;
+          req4_cmd_in = 0;
+          req4_data_in = 0;
+
+          #200
+
+          req3_cmd_in = 0;
+          req3_data_in = 32'b0000_0000_0000_0000_0000_0000_0000_0001;
+
+          #10
+          rd_data3_out = out_data3;
+          rd_resp3_out = out_resp3;
+          if (rd_data3_out != 2 || rd_data3_out != 0) begin
+            $display("Output response should be 2 and is: %d, and answer should be 0 and is %d\n", rd_resp3_out, rd_data3_out);
+            end
+
+          // TEST 8: Overflow error on port 4
+          $display("Test for overflow error on port 4, adding 1 to max\n");
+          #400
+
+            req1_cmd_in = 0;
+          req1_data_in = 0;
+          req2_cmd_in = 0;
+          req2_data_in = 0;
+          req3_cmd_in = 0;
+          req3_data_in = 0;
+          req4_cmd_in = 1;
+          req4_data_in = 32'b1111_1111_1111_1111_1111_1111_1111_1111;
+
+          #200
+
+          req4_cmd_in = 0;
+          req4_data_in = 32'b0000_0000_0000_0000_0000_0000_0000_0001;
+
+          #10
+          rd_data4_out = out_data4;
+          rd_resp4_out = out_resp4;
+          if (rd_data4_out != 2 || rd_data4_out != 0) begin
+            $display("Output response should be 2 and is: %d, and answer should be 0 and is %d\n", rd_resp4_out, rd_data4_out);
+            end
