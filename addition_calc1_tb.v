c1fb2ed5d1619e115b948598dbfff0c076f1daef
Will Rollason <willr292@gmail.com>
2017-10-27 12:44:37 +0100

    arefwrvfdcs

--- addition_calc1_tb.v
+++ addition_calc1_tb.v
@@ -1,4 +1,7 @@
-  $display("SEPERATE FILE Testing addition on each bit");
+  //ADDITION TESTS
+
+  //Test 1 Addition for each bit port1.
+  $display("Testing addition on each bit port 1.");
 
   x = 1;
 
@@ -27,10 +30,127 @@
       if(rd_data1_out != x) begin
         $display("ANSWER WAS NOT CORRECT, out data was %d when answer should be %d\n", rd_data1_out, x);
         end
-
+        /*
       if(rd_data1_out == x) begin
         $display("ANSWER WAS CORRECT, out data was %d and answer is %d\n", rd_data1_out, x);
         end
-
+        */
       x=(x<<1);
     end
+
+    //Test 2 Addition for each bit port2.
+    $display("Testing addition on each bit port 2.");
+
+    x = 1;
+
+    repeat(31) begin
+
+        #200
+
+        req1_cmd_in = 0;
+        req1_data_in = 0;
+        req2_cmd_in = 1;
+        req2_data_in = x;
+        req3_cmd_in = 0;
+        req3_data_in = 0;
+        req4_cmd_in = 0;
+        req4_data_in = 0;
+
+        #200
+
+        req2_cmd_in = 0;
+        req2_data_in = 32'b0000_0000_0000_0000_0000_0000_0000_0000;
+
+        #10
+
+        rd_data2_out = out_data2;
+
+        if(rd_data2_out != x) begin
+          $display("ANSWER WAS NOT CORRECT, out data was %d when answer should be %d\n", rd_data2_out, x);
+          end
+          /*
+        if(rd_data2_out == x) begin
+          $display("ANSWER WAS CORRECT, out data was %d and answer is %d\n", rd_data2_out, x);
+          end
+          */
+
+        x=(x<<1);
+      end
+
+      //Test 3 Addition for each bit port3.
+      $display("Testing addition on each bit port 3.");
+
+      x = 1;
+
+      repeat(31) begin
+
+          #200
+
+          req1_cmd_in = 0;
+          req1_data_in = 0;
+          req2_cmd_in = 0;
+          req2_data_in = 0;
+          req3_cmd_in = 1;
+          req3_data_in = x;
+          req4_cmd_in = 0;
+          req4_data_in = 0;
+
+          #200
+
+          req3_cmd_in = 0;
+          req3_data_in = 32'b0000_0000_0000_0000_0000_0000_0000_0000;
+
+          #10
+
+          rd_data3_out = out_data3;
+
+          if(rd_data3_out != x) begin
+            $display("ANSWER WAS NOT CORRECT, out data was %d when answer should be %d\n", rd_data3_out, x);
+            end
+            /*
+          if(rd_data3_out == x) begin
+            $display("ANSWER WAS CORRECT, out data was %d and answer is %d\n", rd_data3_out, x);
+            end
+            */
+
+          x=(x<<1);
+        end
+
+        //Test 4 Addition for each bit port4.
+        $display("Testing addition on each bit port 4.");
+
+        x = 1;
+
+        repeat(31) begin
+
+            #200
+
+            req1_cmd_in = 0;
+            req1_data_in = 0;
+            req2_cmd_in = 0;
+            req2_data_in = 0;
+            req3_cmd_in = 0;
+            req3_data_in = 0;
+            req4_cmd_in = 1;
+            req4_data_in = x;
+
+            #200
+
+            req4_cmd_in = 0;
+            req4_data_in = 32'b0000_0000_0000_0000_0000_0000_0000_0000;
+
+            #10
+
+            rd_data4_out = out_data4;
+
+            if(rd_data4_out != x) begin
+              $display("ANSWER WAS NOT CORRECT, out data was %d when answer should be %d\n", rd_data4_out, x);
+              end
+              /*
+            if(rd_data4_out == x) begin
+              $display("ANSWER WAS CORRECT, out data was %d and answer is %d\n", rd_data4_out, x);
+              end
+              */
+
+            x=(x<<1);
+          end
