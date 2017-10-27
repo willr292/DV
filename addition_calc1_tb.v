858484101b6962737bbc512cf006b85f1fdd69e7
Will Rollason <willr292@gmail.com>
2017-10-27 13:05:15 +0100

    yjjvgg

--- addition_calc1_tb.v
+++ addition_calc1_tb.v
@@ -1,7 +1,45 @@
-  //ADDITION TESTS
+//ADDITION TESTS
 
-  //Test 1 Addition for each bit port1.
-  $display("Testing addition on each bit port 1.");
+//Test 1 Addition for each bit port1.
+$display("Testing addition on each bit port 1.");
+
+x = 1;
+
+repeat(31) begin
+
+    #200
+
+    req1_cmd_in = 1;
+    req1_data_in = x;
+    req2_cmd_in = 0;
+    req2_data_in = 0;
+    req3_cmd_in = 0;
+    req3_data_in = 0;
+    req4_cmd_in = 0;
+    req4_data_in = 0;
+
+    #200
+
+    req1_cmd_in = 0;
+    req1_data_in = 32'b0000_0000_0000_0000_0000_0000_0000_0000;
+
+    #10
+
+    rd_data1_out = out_data1;
+
+    if(rd_data1_out != x) begin
+      $display("ANSWER WAS NOT CORRECT, out data was %d when answer should be %d\n", rd_data1_out, x);
+      end
+      /*
+    if(rd_data1_out == x) begin
+      $display("ANSWER WAS CORRECT, out data was %d and answer is %d\n", rd_data1_out, x);
+      end
+      */
+    x=(x<<1);
+  end
+
+  //Test 2 Addition for each bit port2.
+  $display("Testing addition on each bit port 2.");
 
   x = 1;
 
@@ -9,10 +47,10 @@
 
       #200
 
-      req1_cmd_in = 1;
-      req1_data_in = x;
-      req2_cmd_in = 0;
-      req2_data_in = 0;
+      req1_cmd_in = 0;
+      req1_data_in = 0;
+      req2_cmd_in = 1;
+      req2_data_in = x;
       req3_cmd_in = 0;
       req3_data_in = 0;
       req4_cmd_in = 0;
@@ -20,26 +58,27 @@
 
       #200
 
-      req1_cmd_in = 0;
-      req1_data_in = 32'b0000_0000_0000_0000_0000_0000_0000_0000;
+      req2_cmd_in = 0;
+      req2_data_in = 32'b0000_0000_0000_0000_0000_0000_0000_0000;
 
       #10
 
-      rd_data1_out = out_data1;
+      rd_data2_out = out_data2;
 
-      if(rd_data1_out != x) begin
-        $display("ANSWER WAS NOT CORRECT, out data was %d when answer should be %d\n", rd_data1_out, x);
+      if(rd_data2_out != x) begin
+        $display("ANSWER WAS NOT CORRECT, out data was %d when answer should be %d\n", rd_data2_out, x);
         end
         /*
-      if(rd_data1_out == x) begin
-        $display("ANSWER WAS CORRECT, out data was %d and answer is %d\n", rd_data1_out, x);
+      if(rd_data2_out == x) begin
+        $display("ANSWER WAS CORRECT, out data was %d and answer is %d\n", rd_data2_out, x);
         end
         */
+
       x=(x<<1);
     end
 
-    //Test 2 Addition for each bit port2.
-    $display("Testing addition on each bit port 2.");
+    //Test 3 Addition for each bit port3.
+    $display("Testing addition on each bit port 3.");
 
     x = 1;
 
@@ -49,36 +88,36 @@
 
         req1_cmd_in = 0;
         req1_data_in = 0;
-        req2_cmd_in = 1;
-        req2_data_in = x;
-        req3_cmd_in = 0;
-        req3_data_in = 0;
+        req2_cmd_in = 0;
+        req2_data_in = 0;
+        req3_cmd_in = 1;
+        req3_data_in = x;
         req4_cmd_in = 0;
         req4_data_in = 0;
 
         #200
 
-        req2_cmd_in = 0;
-        req2_data_in = 32'b0000_0000_0000_0000_0000_0000_0000_0000;
+        req3_cmd_in = 0;
+        req3_data_in = 32'b0000_0000_0000_0000_0000_0000_0000_0000;
 
         #10
 
-        rd_data2_out = out_data2;
+        rd_data3_out = out_data3;
 
-        if(rd_data2_out != x) begin
-          $display("ANSWER WAS NOT CORRECT, out data was %d when answer should be %d\n", rd_data2_out, x);
+        if(rd_data3_out != x) begin
+          $display("ANSWER WAS NOT CORRECT, out data was %d when answer should be %d\n", rd_data3_out, x);
           end
           /*
-        if(rd_data2_out == x) begin
-          $display("ANSWER WAS CORRECT, out data was %d and answer is %d\n", rd_data2_out, x);
+        if(rd_data3_out == x) begin
+          $display("ANSWER WAS CORRECT, out data was %d and answer is %d\n", rd_data3_out, x);
           end
           */
 
         x=(x<<1);
       end
 
-      //Test 3 Addition for each bit port3.
-      $display("Testing addition on each bit port 3.");
+      //Test 4 Addition for each bit port4.
+      $display("Testing addition on each bit port 4.");
 
       x = 1;
 
@@ -90,167 +129,128 @@
           req1_data_in = 0;
           req2_cmd_in = 0;
           req2_data_in = 0;
-          req3_cmd_in = 1;
-          req3_data_in = x;
-          req4_cmd_in = 0;
-          req4_data_in = 0;
+          req3_cmd_in = 0;
+          req3_data_in = 0;
+          req4_cmd_in = 1;
+          req4_data_in = x;
 
           #200
 
-          req3_cmd_in = 0;
-          req3_data_in = 32'b0000_0000_0000_0000_0000_0000_0000_0000;
+          req4_cmd_in = 0;
+          req4_data_in = 32'b0000_0000_0000_0000_0000_0000_0000_0000;
 
           #10
 
-          rd_data3_out = out_data3;
+          rd_data4_out = out_data4;
 
-          if(rd_data3_out != x) begin
-            $display("ANSWER WAS NOT CORRECT, out data was %d when answer should be %d\n", rd_data3_out, x);
+          if(rd_data4_out != x) begin
+            $display("ANSWER WAS NOT CORRECT, out data was %d when answer should be %d\n", rd_data4_out, x);
             end
             /*
-          if(rd_data3_out == x) begin
-            $display("ANSWER WAS CORRECT, out data was %d and answer is %d\n", rd_data3_out, x);
+          if(rd_data4_out == x) begin
+            $display("ANSWER WAS CORRECT, out data was %d and answer is %d\n", rd_data4_out, x);
             end
             */
 
           x=(x<<1);
         end
 
-        //Test 4 Addition for each bit port4.
-        $display("Testing addition on each bit port 4.");
+        // TEST 5: Overflow error on port 1
+      	$display("Test for overflow error on port 1, adding 1 to max\n");
+      	#400
 
-        x = 1;
+      		req1_cmd_in = 1;
+      	req1_data_in = 32'b1111_1111_1111_1111_1111_1111_1111_1111;
+      	req2_cmd_in = 0;
+      	req2_data_in = 0;
+      	req3_cmd_in = 0;
+      	req3_data_in = 0;
+      	req4_cmd_in = 0;
+      	req4_data_in = 0;
 
-        repeat(31) begin
+      	#200
 
-            #200
+      	req1_cmd_in = 0;
+      	req1_data_in = 32'b0000_0000_0000_0000_0000_0000_0000_0001;
 
-            req1_cmd_in = 0;
-            req1_data_in = 0;
-            req2_cmd_in = 0;
-            req2_data_in = 0;
-            req3_cmd_in = 0;
-            req3_data_in = 0;
-            req4_cmd_in = 1;
-            req4_data_in = x;
-
-            #200
-
-            req4_cmd_in = 0;
-            req4_data_in = 32'b0000_0000_0000_0000_0000_0000_0000_0000;
-
-            #10
-
-            rd_data4_out = out_data4;
-
-            if(rd_data4_out != x) begin
-              $display("ANSWER WAS NOT CORRECT, out data was %d when answer should be %d\n", rd_data4_out, x);
-              end
-              /*
-            if(rd_data4_out == x) begin
-              $display("ANSWER WAS CORRECT, out data was %d and answer is %d\n", rd_data4_out, x);
-              end
-              */
-
-            x=(x<<1);
+      	#10
+      	rd_data1_out = out_data1;
+      	rd_resp1_out = out_resp1;
+        if (rd_data1_out != 2 || rd_data1_out != 0) begin
+          $display("Output response should be 2 and is: %d, and answer should be 0 and is %d\n", rd_resp1_out, rd_data1_out);
           end
 
-          // TEST 5: Overflow error on port 1
-        	$display("Test for overflow error on port 1, adding 1 to max\n");
-        	#400
+        // TEST 6: Overflow error on port 2
+      	$display("Test for overflow error on port 2, adding 1 to max\n");
+      	#400
 
-        		req1_cmd_in = 1;
-        	req1_data_in = 32'b1111_1111_1111_1111_1111_1111_1111_1111;
-        	req2_cmd_in = 0;
-        	req2_data_in = 0;
-        	req3_cmd_in = 0;
-        	req3_data_in = 0;
-        	req4_cmd_in = 0;
-        	req4_data_in = 0;
+      		req1_cmd_in = 0;
+      	req1_data_in = 0;
+      	req2_cmd_in = 1;
+      	req2_data_in = 32'b1111_1111_1111_1111_1111_1111_1111_1111;
+      	req3_cmd_in = 0;
+      	req3_data_in = 0;
+      	req4_cmd_in = 0;
+      	req4_data_in = 0;
 
-        	#200
+      	#200
 
-        	req1_cmd_in = 0;
-        	req1_data_in = 32'b0000_0000_0000_0000_0000_0000_0000_0001;
+      	req2_cmd_in = 0;
+      	req2_data_in = 32'b0000_0000_0000_0000_0000_0000_0000_0001;
 
-        	#10
-        	rd_data1_out = out_data1;
-        	rd_resp1_out = out_resp1;
-          if (rd_data1_out != 2 || rd_data1_out != 0) begin
-            $display("Output response should be 2 and is: %d, and answer should be 0 and is %d\n", rd_resp1_out, rd_data1_out);
-            end
+      	#10
+      	rd_data2_out = out_data2;
+      	rd_resp2_out = out_resp2;
+        if (rd_data2_out != 2 || rd_data2_out != 0) begin
+          $display("Output response should be 2 and is: %d, and answer should be 0 and is %d\n", rd_resp2_out, rd_data2_out);
+          end
 
-          // TEST 6: Overflow error on port 2
-        	$display("Test for overflow error on port 2, adding 1 to max\n");
-        	#400
+        // TEST 7: Overflow error on port 3
+        $display("Test for overflow error on port 3, adding 1 to max\n");
+        #400
 
-        		req1_cmd_in = 0;
-        	req1_data_in = 0;
-        	req2_cmd_in = 1;
-        	req2_data_in = 32'b1111_1111_1111_1111_1111_1111_1111_1111;
-        	req3_cmd_in = 0;
-        	req3_data_in = 0;
-        	req4_cmd_in = 0;
-        	req4_data_in = 0;
+          req1_cmd_in = 0;
+        req1_data_in = 0;
+        req2_cmd_in = 0;
+        req2_data_in = 0;
+        req3_cmd_in = 1;
+        req3_data_in = 32'b1111_1111_1111_1111_1111_1111_1111_1111;
+        req4_cmd_in = 0;
+        req4_data_in = 0;
 
-        	#200
+        #200
 
-        	req2_cmd_in = 0;
-        	req2_data_in = 32'b0000_0000_0000_0000_0000_0000_0000_0001;
+        req3_cmd_in = 0;
+        req3_data_in = 32'b0000_0000_0000_0000_0000_0000_0000_0001;
 
-        	#10
-        	rd_data2_out = out_data2;
-        	rd_resp2_out = out_resp2;
-          if (rd_data2_out != 2 || rd_data2_out != 0) begin
-            $display("Output response should be 2 and is: %d, and answer should be 0 and is %d\n", rd_resp2_out, rd_data2_out);
-            end
+        #10
+        rd_data3_out = out_data3;
+        rd_resp3_out = out_resp3;
+        if (rd_data3_out != 2 || rd_data3_out != 0) begin
+          $display("Output response should be 2 and is: %d, and answer should be 0 and is %d\n", rd_resp3_out, rd_data3_out);
+          end
 
-          // TEST 7: Overflow error on port 3
-          $display("Test for overflow error on port 3, adding 1 to max\n");
-          #400
+        // TEST 8: Overflow error on port 4
+        $display("Test for overflow error on port 4, adding 1 to max\n");
+        #400
 
-            req1_cmd_in = 0;
-          req1_data_in = 0;
-          req2_cmd_in = 0;
-          req2_data_in = 0;
-          req3_cmd_in = 1;
-          req3_data_in = 32'b1111_1111_1111_1111_1111_1111_1111_1111;
-          req4_cmd_in = 0;
-          req4_data_in = 0;
+          req1_cmd_in = 0;
+        req1_data_in = 0;
+        req2_cmd_in = 0;
+        req2_data_in = 0;
+        req3_cmd_in = 0;
+        req3_data_in = 0;
+        req4_cmd_in = 1;
+        req4_data_in = 32'b1111_1111_1111_1111_1111_1111_1111_1111;
 
-          #200
+        #200
 
-          req3_cmd_in = 0;
-          req3_data_in = 32'b0000_0000_0000_0000_0000_0000_0000_0001;
+        req4_cmd_in = 0;
+        req4_data_in = 32'b0000_0000_0000_0000_0000_0000_0000_0001;
 
-          #10
-          rd_data3_out = out_data3;
-          rd_resp3_out = out_resp3;
-          if (rd_data3_out != 2 || rd_data3_out != 0) begin
-            $display("Output response should be 2 and is: %d, and answer should be 0 and is %d\n", rd_resp3_out, rd_data3_out);
-            end
-
-          // TEST 8: Overflow error on port 4
-          $display("Test for overflow error on port 4, adding 1 to max\n");
-          #400
-
-            req1_cmd_in = 0;
-          req1_data_in = 0;
-          req2_cmd_in = 0;
-          req2_data_in = 0;
-          req3_cmd_in = 0;
-          req3_data_in = 0;
-          req4_cmd_in = 1;
-          req4_data_in = 32'b1111_1111_1111_1111_1111_1111_1111_1111;
-
-          #200
-
-          req4_cmd_in = 0;
-          req4_data_in = 32'b0000_0000_0000_0000_0000_0000_0000_0001;
-
-          #10
-          rd_data4_out = out_data4;
-          rd_resp4_out = out_resp4;
-          if (rd_data4_out != 2 || rd_data4_out != 0) begin
-            $display("Output response should be 2 and is: %d, and answer should be 0 and is %d\n", rd_resp4_out, rd_data4_out);
-            end
+        #10
+        rd_data4_out = out_data4;
+        rd_resp4_out = out_resp4;
+        if (rd_data4_out != 2 || rd_data4_out != 0) begin
+          $display("Output response should be 2 and is: %d, and answer should be 0 and is %d\n", rd_resp4_out, rd_data4_out);
+          end
---
+++ leftshift_calc1_tb.v
@@ -0,0 +1,202 @@
+//LEFT SHIFT TESTS
+
+// TEST 1 Left Shift 1 bit for each bit on port 1.
+
+	#400
+
+	$display("Testing left shift on each bit port 1");
+
+	y = 1;
+
+	repeat(31) begin
+
+			#200
+
+			req1_cmd_in = 5;
+			req1_data_in = y;
+			req2_cmd_in = 0;
+			req2_data_in = 0;
+			req3_cmd_in = 0;
+			req3_data_in = 0;
+			req4_cmd_in = 0;
+			req4_data_in = 0;
+
+			#200
+
+			req1_cmd_in = 0;
+			req1_data_in = 32'b0000_0000_0000_0000_0000_0000_0000_0001;
+
+			#100
+
+			rd_data1_out = out_data1;
+
+			if(rd_data1_out != (y<<1)) begin
+				$display("ANSWER WAS NOT CORRECT, out data was %d when answer should be %d\n", $unsigned(rd_data1_out), $unsigned(y<<1));
+				end
+        /*
+			if(rd_data1_out == (y<<1)) begin
+				$display("ANSWER WAS CORRECT, out data was %d and answer is %d\n", $unsigned(rd_data1_out), $unsigned(y<<1));
+				end
+        */
+			y=(y<<1);
+		end
+
+// TEST 2 Left Shift 1 bit for each bit on port 2.
+
+	#400
+
+	$display("Testing left shift on each bit port 2");
+
+	y = 1;
+
+	repeat(31) begin
+
+			#200
+
+			req1_cmd_in = 0;
+			req1_data_in = 0;
+			req2_cmd_in = 5;
+			req2_data_in = y;
+			req3_cmd_in = 0;
+			req3_data_in = 0;
+			req4_cmd_in = 0;
+			req4_data_in = 0;
+
+			#200
+
+			req2_cmd_in = 0;
+			req2_data_in = 32'b0000_0000_0000_0000_0000_0000_0000_0001;
+
+			#100
+
+			rd_data2_out = out_data2;
+
+			if(rd_data2_out != (y<<1)) begin
+				$display("ANSWER WAS NOT CORRECT, out data was %d when answer should be %d\n", $unsigned(rd_data2_out), $unsigned(y<<1));
+				end
+        /*
+			if(rd_data2_out == (y<<1)) begin
+				$display("ANSWER WAS CORRECT, out data was %d and answer is %d\n", $unsigned(rd_data2_out), $unsigned(y<<1));
+				end
+        */
+			y=(y<<1);
+		end
+
+// TEST 3 Left Shift 1 bit for each bit on port 3.
+
+  #400
+
+  $display("Testing left shift on each bit port 3");
+
+  y = 1;
+
+  repeat(31) begin
+
+      #200
+
+      req1_cmd_in = 0;
+      req1_data_in = 0;
+      req2_cmd_in = 0;
+      req2_data_in = 0;
+      req3_cmd_in = 5;
+      req3_data_in = y;
+      req4_cmd_in = 0;
+      req4_data_in = 0;
+
+      #200
+
+      req3_cmd_in = 0;
+      req3_data_in = 32'b0000_0000_0000_0000_0000_0000_0000_0001;
+
+      #100
+
+      rd_data3_out = out_data3;
+
+      if(rd_data3_out != (y<<1)) begin
+        $display("ANSWER WAS NOT CORRECT, out data was %d when answer should be %d\n", $unsigned(rd_data3_out), $unsigned(y<<1));
+        end
+        /*
+      if(rd_data3_out == (y<<1)) begin
+        $display("ANSWER WAS CORRECT, out data was %d and answer is %d\n", $unsigned(rd_data3_out), $unsigned(y<<1));
+        end
+        */
+      y=(y<<1);
+    end
+
+// TEST 4 Left Shift 1 bit for each bit on port 4.
+
+  #400
+
+  $display("Testing left shift on each bit port 4");
+
+  y = 1;
+
+  repeat(31) begin
+
+      #200
+
+      req1_cmd_in = 0;
+      req1_data_in = 0;
+      req2_cmd_in = 0;
+      req2_data_in = 0;
+      req3_cmd_in = 0;
+      req3_data_in = 0;
+      req4_cmd_in = 5;
+      req4_data_in = y;
+
+      #200
+
+      req4_cmd_in = 0;
+      req4_data_in = 32'b0000_0000_0000_0000_0000_0000_0000_0001;
+
+      #100
+
+      rd_data4_out = out_data4;
+
+      if(rd_data4_out != (y<<1)) begin
+        $display("ANSWER WAS NOT CORRECT, out data was %d when answer should be %d\n", $unsigned(rd_data4_out), $unsigned(y<<1));
+        end
+        /*
+      if(rd_data4_out == (y<<1)) begin
+        $display("ANSWER WAS CORRECT, out data was %d and answer is %d\n", $unsigned(rd_data4_out), $unsigned(y<<1));
+        end
+        */
+      y=(y<<1);
+    end
+
+
+//Test 2 left shift by increasing amount
+  $display("Testing left shift by increasing amount port 1");
+
+  a = 1;
+
+  repeat(31) begin
+
+  req1_cmd_in = 5;
+  req1_data_in = 1;
+  req2_cmd_in = 0;
+  req2_data_in = 0;
+  req3_cmd_in = 0;
+  req3_data_in = 0;
+  req4_cmd_in = 0;
+  req4_data_in = 0;
+
+  #200
+
+  req1_cmd_in = 0;
+  req1_data_in = a;
+
+  #200
+
+  rd_data1_out = out_data1;
+
+  if(rd_data1_out != (1<<a)) begin
+    $display("ANSWER WAS NOT CORRECT, out data was %d when answer should be %d\n", $unsigned(rd_data1_out), (1<<a));
+    end
+    
+  if(rd_data1_out == (1<<a)) begin
+    $display("ANSWER WAS CORRECT, out data was %d and answer is %d\n", $unsigned(rd_data1_out), (1<<a));
+    end
+
+  a=a+1;
+end
